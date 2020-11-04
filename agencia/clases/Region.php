26672d4472fb7bf26f343d96d945522fc990a14a
marcos pinardi <exegeses@gmail.com>
2020-11-04 12:17:12 -0300

    gettes & setters en clase Region

--- agencia/clases/Region.php
+++ agencia/clases/Region.php
@@ -2,5 +2,40 @@
 
     class Region
     {
+        private $regID;
+        private $regNombre;
 
+
+
+        ###############################
+        ##### getters & setters #######
+        /**
+         * @return mixed
+         */
+        public function getRegID()
+        {
+            return $this->regID;
+        }
+        /**
+         * @param mixed $regID
+         */
+        public function setRegID($regID): void
+        {
+            $this->regID = $regID;
+        }
+
+        /**
+         * @return mixed
+         */
+        public function getRegNombre()
+        {
+            return $this->regNombre;
+        }
+        /**
+         * @param mixed $regNombre
+         */
+        public function setRegNombre($regNombre): void
+        {
+            $this->regNombre = $regNombre;
+        }
     }
