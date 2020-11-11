977ddaa3f4256348aa3e1da0fe54378f0c526e6d
marcos pinardi <exegeses@gmail.com>
2020-11-11 11:10:04 -0300

    panel de adm,inistración de regiones usando método listarRegiones()

--- agencia/clases/Region.php
+++ agencia/clases/Region.php
@@ -5,7 +5,15 @@
         private $regID;
         private $regNombre;
 
-
+        public function listarRegiones()
+        {
+            $link = Conexion::conectar();
+            $sql = "SELECT regID, regNombre FROM regiones";
+            $stmt = $link->prepare($sql);
+            $stmt->execute();
+            $regiones = $stmt->fetchAll(PDO::FETCH_ASSOC);
+            return $regiones;
+        }
 
         ###############################
         ##### getters & setters #######
